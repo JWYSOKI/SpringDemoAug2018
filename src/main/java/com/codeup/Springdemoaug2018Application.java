f6658c9d9f0056e246563be9e594087b2d91b50c
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-23 18:29:08 -0500

    inital commit

---
+++ src/main/java/com/codeup/Springdemoaug2018Application.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemoaug2018Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdemoaug2018Application.class, args);
+    }
+}
